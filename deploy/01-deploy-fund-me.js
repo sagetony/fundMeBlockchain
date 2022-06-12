96cd09b9455354a51a9e85bc2045cde1299e99af
Sagetony <uchetony224@gmail.com>
2022-06-12 11:24:53 +0100

    first deployment

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,6 @@
+module.exports = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+}
